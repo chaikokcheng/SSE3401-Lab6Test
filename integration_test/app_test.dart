08138466122612e2b1af1b0875b443eeb3469060
Chai Kok Cheng <94617307+chaikokcheng@users.noreply.github.com>
2024-05-04 02:49:11 +0800

    testupdate and firebase

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,79 +7,40 @@ import 'package:mad_lab5/main.dart' as app;
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Onboarding Integration Tests', () {
-    testWidgets('Complete onboarding process', (WidgetTester tester) async {
-      app.main();
-      await tester.pumpAndSettle();
+  testWidgets('Complete onboarding process', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
 
-      // Verify the onboarding page is shown
-      expect(find.text('Welcome !'), findsOneWidget);
+    // Verify the onboarding page is shown
+    expect(find.text('Welcome !'), findsOneWidget);
 
-      // Enter the phone number
-      await tester.enterText(find.byType(TextField).first, '109996666');
-      await Future.delayed(const Duration(seconds: 1));
-      await tester.pumpAndSettle();
+    // Enter the phone number and check the checkbox
+    await tester.enterText(find.byType(TextField).first, '109996666');
+    await tester.pumpAndSettle();
+    await tester.tap(find.byType(Checkbox));
+    await tester.pumpAndSettle();
 
-      // Check the checkbox to agree to the terms
-      await tester.tap(find.byType(Checkbox));
-      await tester.pumpAndSettle();
+    // Tap on the 'Get Activation Code' button
+    await tester
+        .tap(find.widgetWithText(ElevatedButton, 'Get Activation Code'));
+    await tester.pumpAndSettle();
 
-      // Tap on the 'Get Activation Code' button
-      await tester
-          .tap(find.widgetWithText(ElevatedButton, 'Get Activation Code'));
-      await tester.pumpAndSettle();
+    // Check if the OTP input is visible
+    expect(find.text('Enter the activation code you received via SMS.'),
+        findsOneWidget);
 
-      // Assume an OTP is entered and the 'Activate' button appears and is functional
-      await tester.enterText(find.byType(TextField).last, '885566');
-      await Future.delayed(const Duration(seconds: 1));
-      await tester.pumpAndSettle();
+    // Enter an OTP
+    await tester.enterText(find.byType(TextField).last, '885566');
+    await tester.pumpAndSettle();
 
-      // Tap on 'Activate' button
-      await tester.tap(find.text('Activate'));
-      await tester.pumpAndSettle();
+    // Ensure 'Activate' button is visible before attempting to tap
+    expect(find.text('Activate'), findsOneWidget);
 
-      // Verify navigation to HomePage
-      expect(find.byType(HomePage), findsOneWidget);
-    });
-    // testWidgets('Complete flow from HomePage to submitting contact',
-    //     (WidgetTester tester) async {
-    //   // Build our app and trigger a frame.
-    //   await tester.pumpWidget(MaterialApp(home: HomePage()));
+    // Tap on 'Activate' button
+    await tester.tap(find.text('Activate'));
+    await tester.pumpAndSettle();
 
-    //   // Find the FloatingActionButton by Icon and tap it to navigate to AddContactPage.
-    //   final addButtonFinder =
-    //       find.widgetWithIcon(FloatingActionButton, Icons.add);
-    //   expect(addButtonFinder, findsOneWidget);
-    //   await tester.tap(addButtonFinder);
-    //   await tester.pumpAndSettle(); // Wait for page transition
-
-    //   // Find TextFields for name and phone number and enter some text.
-    //   final nameField = find.widgetWithText(TextField, 'Type here');
-    //   final phoneField =
-    //       find.widgetWithText(TextField, 'Enter your phone number');
-
-    //   expect(nameField, findsOneWidget);
-    //   expect(phoneField, findsOneWidget);
-
-    //   // Enter text into the TextFields
-    //   await tester.enterText(nameField, 'John Doe');
-    //   await tester.enterText(phoneField, '+1234567890');
-    //   await tester.pump(); // Rebuild the widget with the new text.
-
-    //   // Tap the 'Submit' button
-    //   final submitButtonFinder = find.widgetWithText(ElevatedButton, 'Submit');
-    //   await tester.tap(submitButtonFinder);
-    //   await tester.pumpAndSettle(); // Wait for any animations
-
-    //   // Expect the AlertDialog to appear after submission
-    //   expect(find.byType(AlertDialog), findsOneWidget);
-    //   expect(find.text('Success'), findsOneWidget);
-    //   expect(find.text('You had done invited'), findsOneWidget);
-
-    //   // Tap the 'Done' button on the AlertDialog
-    //   final doneButtonFinder = find.widgetWithText(TextButton, 'Done');
-    //   await tester.tap(doneButtonFinder);
-    //   await tester.pumpAndSettle(); // Dismiss the AlertDialog
-    // });
+    // Verify navigation to HomePage
+    expect(find.byType(HomePage), findsOneWidget);
   });
 }
